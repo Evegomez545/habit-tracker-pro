ff8cdf3abb489718587fdc0f649061a1de6a3407
Evegomez545 <evygomez56@gmail.com>
2026-02-16 06:54:56 -0300

    Configurando UserDetailsService para validar login no banco

--- habit-tracker/src/main/java/com/evelyn/habit_tracker/SecurityConfig.java
+++ habit-tracker/src/main/java/com/evelyn/habit_tracker/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.evelyn.habit_tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,5 +34,21 @@ public class SecurityConfig {
             .logout(logout -> logout.permitAll());
 
         return http.build();
+    }
+
+   
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            
+            Usuario usuario = usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+            
+            
+            return User.withUsername(usuario.getEmail())
+                .password(usuario.getSenha())
+                .authorities("USER")
+                .build();
+        };
     }
 }
